7684211a098c68258f04424c88f4f827b3b4c928
Vasil Rusinov <114298909+rusinovvasil@users.noreply.github.com>
2023-03-05 10:04:00 +0100

    JS Applications - Asynchronous Programming
    Add solutions for exercise

---
+++ JS Application Feb2023/02. JS-Application Assynchronous Programming/Exercise/02.Bus-Schedule/app.js
@@ -0,0 +1,44 @@
+function solve() {
+    const infoElement = document.querySelector('div#info span.info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+    let nextStopId = 'depot';
+    let stopName;
+
+    async function depart() {
+        try {
+            const res = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`)
+            if (!res.ok) {
+                let error = new Error();
+                error.status = response.status;
+                error.statusText = response.statusText;
+                throw error;
+            }
+            const data = await res.json();
+            stopName = data.name;
+            nextStopId = data.next;
+            infoElement.textContent = `Next stop ${stopName}`
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        }
+        catch (error) {
+            infoElement.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+    }
+
+    function arrive() {
+        infoElement.textContent = `Arriving at ${stopName}`
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
+
+let result = solve();
